7ad97518760d2160f8e0f52a3b9478a933ca7b52
hossein <shahrokhi.psm@gmail.com>
2021-04-20 22:38:40 +0430

    fibonacci algorithm completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let n = 6;
+    println!("{}th number of fibonacci is {}",n,fibonacci_number(n));
+    
+}
+fn fibonacci_number(n: i32)-> i32 {
+    let mut first = 1;
+    let mut second = 1;
+    let mut third = first + second;
+    let mut counter = 4;
+    while counter <= n {
+        first = second;
+        second = third;
+        third = first + second;
+        counter += 1;
+    }
+    third
+
 }
